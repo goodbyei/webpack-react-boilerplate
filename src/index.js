5bd7e57ec780af3195633811b67ae092a1fa74d7
goodbyei <goodbyei@protonmail.com>
2020-02-18 17:37:50 +0300

    Added React

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './components/App'
+
 import './styles/main.css'
 
-console.log('Initial...')
+ReactDOM.render(<App />, document.getElementById("react-root"))
+
--- webpack.config.js
+++ webpack.config.js
@@ -52,6 +52,7 @@ module.exports = {
           options: {
             presets: [
               '@babel/preset-env',
+              '@babel/preset-react',
             ]
           }
         }
